9d2ba872ad773cdd0ccd0019e8e13e2505ac3ef7
Murray0575 <notcoldheike.com>
2017-11-04 19:34:42 +0800

    push JupyterNotebookFile

---
+++ JupyterNotebookFile/media1.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+class Movie():
+    def __init__(self,movie_title,movie_storyline,poster_image,trailer_movie):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url=poster_image
+        self.trailer_movie= trailer_movie
+
